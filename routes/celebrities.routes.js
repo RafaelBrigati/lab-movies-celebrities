335bea6a91e4f179e013ccc94855bc1ccea59da1
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-06-10 09:51:41 +0200

    In progress

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,13 @@
+
+const router = require("express").Router();
+
+router.get('/celebrities', (req, res, next) => {
+
+});
+
+
+router.post('/celebrities', (req, res, next) => {
+
+});
+
+module.exports = router;
---
+++ routes/movies.routes.js
@@ -0,0 +1,13 @@
+
+const router = require("express").Router();
+
+router.get('/movies', (req, res, next) => {
+
+});
+
+
+router.post('/movies', (req, res, next) => {
+
+});
+
+module.exports = router;
